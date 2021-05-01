1fba32adc69490e5e06c59232156bb1376dd1d4d
Aidan Shirey <aidan.shirey@gmail.com>
2021-04-30 23:23:09 -0700

    Testing Codepoint Functions

--- content.js
+++ content.js
@@ -1,2 +1,7 @@
 // Base replace script for any emojis already on the page when loaded
-document.body.innerHTML = document.body.innerHTML.replace(/hello/g, 'hi');
+document.body.innerHTML = document.body.innerHTML.replace(/(hello|goodbye|wave|waving)/gi, String.fromCodePoint('0x1F44B'));
+document.body.innerHTML = document.body.innerHTML.replace(/(happy)/gi, String.fromCodePoint('0x1F600'));
+document.body.innerHTML = document.body.innerHTML.replace(/(smiling|smile)/gi, String.fromCodePoint('0x1F603'));
+document.body.innerHTML = document.body.innerHTML.replace(/(xd)/gi, String.fromCodePoint('0x1F606'));
+document.body.innerHTML = document.body.innerHTML.replace(/embarassed/gi, String.fromCodePoint('0x1F605'));
+document.body.innerHTML = document.body.innerHTML.replace(/(simp|begging|beg|pleading|plead)/gi, String.fromCodePoint('0x1F97A'));
